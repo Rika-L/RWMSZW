3090b85b8db3dc0a480a569c5224734b58d7c7d6
xf <14182909+xh_13_0@user.noreply.gitee.com>
2024-10-22 21:14:09 +0800

    首页搜索功能的实现

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,18 @@
 <script lang="ts" setup>
 import data from '@/main.json'
 
+const keyword = ref('') // 定义keyword变量
+const filteredData = ref(data) // 存储过滤后的数据
+
+function filterData() {
+  if (!keyword.value) {
+    filteredData.value = data // 如果keyword为空，重置数据
+  }
+  else {
+    filteredData.value = data.filter(item => item.chinese.includes(keyword.value)) // 过滤数据
+  }
+}
+
 function hdlTap(index: number) {
   uni.navigateTo({
     url: `/pages/word/word?index=${index}`,
@@ -12,22 +24,22 @@ function hdlTap(index: number) {
 <template>
   <view class="fixed left-0 top-0 -z-10 size-full backdrop-blur" />
   <image src="/src/static/img/bg2.jpg" class="fixed left-0 top-0 -z-20 size-full" />
-  <div class=" relative mt-[100rpx] flex justify-center">
-    <input type="text" class=" relative top-0 h-[70rpx] w-[430rpx] rounded-md bg-slate-200 pl-2 text-[26rpx] font-bold text-black opacity-75 shadow-inner shadow-[#888888]" placeholder="请输入想要搜索的文字" bindinput="inputChange">
-    <div class=" absolute left-[607rpx] top-0 h-[70rpx] w-[91rpx] rounded-lg bg-inherit">
-      <div class=" size-full rounded-lg bg-slate-600 opacity-55" />
-      <text class=" absolute left-[10px] top-[19rpx] text-[26rpx] font-semibold" bindtap="search">
+  <div class=" relative ml-[125rpx] mt-[100rpx] flex">
+    <input v-model="keyword" type="text" class=" relative top-0 h-[70rpx] w-[430rpx] rounded-md bg-slate-200 pl-2 text-[26rpx] font-bold text-black opacity-75 shadow-inner shadow-[#888888]" placeholder="请输入想要搜索的文字" bindinput="inputChange" @keyup.enter="filterData">
+    <view class=" absolute left-[457rpx] top-0 h-[70rpx] w-[91rpx] rounded-lg bg-inherit" @tap="filterData">
+      <div class=" size-full rounded-lg bg-slate-600 opacity-15" />
+      <text class=" absolute left-[10px] top-[19rpx] text-[26rpx] font-semibold">
         搜索
       </text>
-    </div>
+    </view>
   </div>
   <scroll-view
-    class=" absolute ml-[90rpx] mt-[50rpx] flex h-[900rpx] w-[580rpx] flex-wrap" scroll-x="false" scroll-y="true"
+    class=" absolute ml-[90rpx] mt-[50rpx] flex h-[900rpx] w-full flex-wrap" scroll-x="false" scroll-y="true"
   >
     <view
-      v-for="(item, index) in data"
+      v-for="(item, index) in filteredData"
       :key="index"
-      class=" relative left-[20rpx] mt-[30rpx] h-[150rpx] w-[560rpx] rounded-[30rpx] border-none bg-inherit"
+      class=" relative left-[20rpx] mt-[30rpx] h-[150rpx] w-[580rpx] rounded-[30rpx] border-none bg-inherit"
       @tap="hdlTap(index)"
     >
       <div class="absolute h-full w-[550rpx] rounded-[30rpx] bg-slate-700 opacity-45" />
