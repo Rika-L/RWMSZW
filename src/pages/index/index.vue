cd64df2069275d284174641f417d9aae04da591f
xf <14182909+xh_13_0@user.noreply.gitee.com>
2024-10-23 09:34:19 +0800

    首页搜索功能的完善和页面优化

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -13,6 +13,12 @@ function filterData() {
   }
 }
 
+watchEffect(() => {
+  if (!keyword.value) {
+    filteredData.value = data
+  }
+})
+
 function hdlTap(index: number) {
   uni.navigateTo({
     url: `/pages/word/word?index=${index}`,
@@ -25,8 +31,8 @@ function hdlTap(index: number) {
   <view class="fixed left-0 top-0 -z-10 size-full backdrop-blur" />
   <image src="/src/static/img/bg2.jpg" class="fixed left-0 top-0 -z-20 size-full" />
   <div class=" relative ml-[125rpx] mt-[100rpx] flex">
-    <input v-model="keyword" type="text" class=" relative top-0 h-[70rpx] w-[430rpx] rounded-md bg-slate-200 pl-2 text-[26rpx] font-bold text-black opacity-75 shadow-inner shadow-[#888888]" placeholder="Please enter the Chinese" bindinput="inputChange" @keyup.enter="filterData">
-    <view class=" absolute left-[457rpx] top-0 h-[70rpx] w-[91rpx] rounded-lg bg-inherit" @tap="filterData">
+    <input v-model="keyword" type="text" class=" relative top-0 h-[70rpx] w-[430rpx] rounded-md bg-slate-200 pl-2 text-[26rpx] font-bold text-black opacity-75 shadow-inner shadow-[#888888]" placeholder="Please enter the Chinese" bindinput="inputChange" @confirm="filterData">
+    <view class=" absolute left-[457rpx] top-0 h-[70rpx] w-[110rpx] rounded-lg bg-inherit" @tap="filterData">
       <div class=" size-full rounded-lg bg-slate-600 opacity-15" />
       <text class=" absolute left-[10px] top-[19rpx] text-[26rpx] font-semibold">
         Search
@@ -44,7 +50,7 @@ function hdlTap(index: number) {
     >
       <div class="absolute h-full w-[550rpx] rounded-[30rpx] bg-slate-700 opacity-45" />
       <div class=" absolute left-[20rpx] top-[15rpx] size-[120rpx] rounded-[30rpx] bg-gray-200 opacity-75">
-        <text class=" absolute  left-[18rpx] top-[-2rpx] text-[90rpx]" selectable="false" space="false" decode="false">
+        <text class=" flex items-center justify-center text-[90rpx]" selectable="false" space="false" decode="false">
           {{ item.chinese }}
         </text>
       </div>
