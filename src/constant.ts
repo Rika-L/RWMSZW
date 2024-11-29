61c11253e4e37aaabde80f7a307416f6af9b7378
xf <14182909+xh_13_0@user.noreply.gitee.com>
2024-11-29 11:55:02 +0800

    组词

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,4 @@
 // const IMG_BASE_URL = 'http://rika00126.a1.luyouxia.net:23958/static/'
-const IMG_BASE_URL = 'http://127.0.0.1:7452/static/'
+const IMG_BASE_URL = 'http://connection.a1.luyouxia.net:29228/static/'
 
 export { IMG_BASE_URL }
